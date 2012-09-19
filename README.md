90332336e76c613ccacffe7ffa86131b50bd37a6
William Blanchette <wb@collectivecognition.com>
2012-09-19 16:53:52 -0600

    Added the ability to specify append/insert mode.

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ jquery.template.js
 
 A ridiculously simple templating library for jQuery.
 
-Usage:
-======
+APPEND:
+=======
 
     $(el).template("<h1>${album}</h1><h2>${artist}</h2><img src='images/${cover}", 
     {album: "Dark Side Of The Moon", artist: "Pink Floyd", "dark-side-of-the-moon.png"});
+
+INSERT:
+=======
+
+    $(el).template("${foo}", {foo: "bar"}, true);
--- jquery.template.js
+++ jquery.template.js
@@ -5,15 +5,17 @@
  *
  * Copyright (c) 2012 William Blanchette, wb@collectivecognition.com
  *
- * USAGE:
+ * APPEND:
  * $(el).template("<h1>${album}</h1><h2>${artist}</h2><img src='images/${cover}", 
  * {album: "Dark Side Of The Moon", artist: "Pink Floyd", "dark-side-of-the-moon.png"});
  *
+ * INSERT: 
+ * $(el).template("${foo}", {foo: "bar"}, true);
  */
 
 (function($){
-	$.fn.template = function(string, obj){
-		return $(this).html(string.replace(/\${(.*?)\}/ig, function(match){
+	$.fn.template = function(string, obj, overwrite){
+		return $(this)[overwrite ? "html" : "append"](string.replace(/\${(.*?)\}/ig, function(match){
 			return obj[match.replace(/[\${\}]/g, "")];
 		}));
 	}
